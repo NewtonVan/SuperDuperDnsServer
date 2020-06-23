3c53249acec32eece2a908d64bc7bd3cf008353a
NewtonVan <48061231+NewtonVan@users.noreply.github.com>
2020-06-23 11:40:49 +0800

    set auto begin script

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+make
+make clean
+sudo ./SuperDuperServer
+
+# actually, just input the command in the shell cost the counts of 
+# commands as using shell script, because you have to chmod...
